94cdee228dfa42c7af4a4d95c865f35ef301aacf
djassoch-tech <d.jasso.ch@gmail.com>
2026-06-22 19:05:47 -0700

    Add files via upload

---
+++ asume_dotic analisys.py
@@ -0,0 +1,11 @@
+arr = [10,20,30,40,50]
+print(arr[0])
+
+target =10
+for x in arr:
+    if x== target: break
+target = 50
+for x in arr:
+    if x == target: break
+for i in arr:
+    for j in arr:pass
